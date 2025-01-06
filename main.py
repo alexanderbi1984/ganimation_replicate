b79aa82f9b0c0413063c6cbd8567df449ef64d9d
alexanderbi1984 <alexanderbi1984@gmail.com>
2025-01-05 19:52:40 -0500

    create a video generator class

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Created on Dec 13, 2018
 from options import Options
 from solvers import create_solver
 from video_generator import VideoGenerator
+import pandas as pd
 
 
 
@@ -14,7 +15,8 @@ if __name__ == '__main__':
     opt = Options().parse()
     if opt.mode == 'generate':
         instance = VideoGenerator(opt)
-        instance.generate_images(opt.src_img_path, opt.tar_aus)
+        aus = pd.read_csv(opt.tar_aus_path)[2:19].values
+        instance.generate_images(opt.src_img_path, aus)
     else:
         solver = create_solver(opt)
         solver.run_solver()
